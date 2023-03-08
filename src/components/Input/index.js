cfa4bdde1fef3ce8b7734ce3558f38541b383eff
buttowsk <vinicius_nunes0104@hotmail.com>
2023-03-07 22:54:21 -0300

    esqueleto

---
+++ src/components/Input/index.js
@@ -0,0 +1,9 @@
+import {InputContainer, InputText} from "./styles";
+
+export const Input = () => {
+    return (
+        <InputContainer>
+            <InputText />
+        </InputContainer>
+    )
+}
---
+++ src/components/Input/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+`
+export const InputText = styled.input`
+  width: 100%;
+`
