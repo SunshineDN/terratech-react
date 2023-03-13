5a12256815a3c3d6e28143a940f512c20b524ccf
Douglas Augusto <100153142+SunshineDN@users.noreply.github.com>
2023-03-13 02:36:44 -0300

    Adicionado navigate

--- src/hooks/useLoginVerification/index.js
+++ src/hooks/useLoginVerification/index.js
@@ -1,12 +1,15 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
+import {useNavigate} from "react-router-dom";
 
 export const useLoginVerification = (setIsLoggedIn) => {
+    const navigate = useNavigate();
     const logged = sessionStorage.getItem('auth');
     return(
         useEffect(() => {
             setTimeout(() => {
                 if (logged !== null){
                     setIsLoggedIn(true);
+                    navigate('/home')
                 } else {
                     setIsLoggedIn(false);
                     console.log("Failed to log in");
