a404063110eb4330a8876524fb7457fc62ac34f7
Douglas Augusto <douglasaugusto1103@gmail.com>
2023-03-13 11:35:32 -0300

    Alterado clientId

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import App from './App';
 import {GlobalStyle} from "./globalStyles/styles";
 import {GoogleOAuthProvider} from "@react-oauth/google";
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const clientId = process.env.REACT_APP_CLIENT_ID
+// const clientId = process.env.REACT_APP_CLIENT_ID
+const clientId = "180197040960-9hdue6iaqlj870r29751hd2l1aj5r4mq.apps.googleusercontent.com"
 root.render(
     <React.StrictMode>
         <GlobalStyle />
