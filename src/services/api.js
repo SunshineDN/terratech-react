49186d606de4818f74c6a61b79c2a78a65c92aa4
Douglas Augusto <douglasaugusto1103@gmail.com>
2023-03-13 11:11:55 -0300

    Alterado variaveis de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: "https://backend-terratech-production.up.railway.app",
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
