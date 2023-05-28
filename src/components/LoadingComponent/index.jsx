5da772b07bf99ccfa13f0a2eb0bb8994b68553c7
buttowsk <vinicius_nunes0104@hotmail.com>
2023-05-28 15:51:59 -0300

    Loading

---
+++ src/components/LoadingComponent/index.jsx
@@ -0,0 +1,9 @@
+import { LoadingContainer, LoadingImg } from './styles.js';
+
+const LoadingComponent = () => {
+  return (
+    <LoadingContainer>
+      <LoadingImg src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="loading"/>
+    </LoadingContainer>
+  );
+};
