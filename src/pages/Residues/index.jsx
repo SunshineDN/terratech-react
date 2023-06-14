7d93d39e65eb61ad886061f2fa7c6ba60ea98840
buttowsk <vinicius_nunes0104@hotmail.com>
2023-06-14 16:01:42 -0300

    residues

---
+++ src/pages/Residues/index.jsx
@@ -0,0 +1,9 @@
+import { Layout } from '../../layout';
+
+export const Residues = () => {
+  return (
+    <Layout>
+      <h1>Residues</h1>
+    </Layout>
+  );
+}
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,6 +3,7 @@ import Register from '../pages/Register/Register';
 import Login from '../pages/Login/Login';
 import PrivateRoutes from './PrivateRoutes.jsx';
 import { Home } from '../pages/Home/Home.jsx';
+import { Residues } from '../pages/Residues/index.jsx';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Route path="/login" element={ <Login/> }/>
       <Route element={ <PrivateRoutes/> }>
         <Route path="/" element={ <Home/> }/>
+        <Route path="/residues" element={ <Residues/> }/>
       </Route>
 
       <Route path="*" element={ <h1>Not Found</h1> }/>
